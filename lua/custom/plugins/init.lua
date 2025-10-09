81fce1a582c6492ea4cd836cb8edda1cde259208
Chris Moseley <cmoseley@datarecognitioncorp.com>
2025-10-09 18:26:49 -0500

    adding custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,22 +3,6 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  'kdheepak/lazygit.nvim',
-  lazy = true,
-  cmd = {
-    'LazyGit',
-    'LazyGitConfig',
-    'LazyGitCurrentFile',
-    'LazyGitFilter',
-    'LazyGitFilterCurrentFile',
-  },
-  -- optional for floating window border decoration
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  keys = {
-    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-  },
+  require 'custom.plugins.lazygit',
+  require 'custom.plugins.typescript-tools',
 }
